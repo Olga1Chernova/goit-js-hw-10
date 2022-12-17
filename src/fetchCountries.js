c7586ab5753ce5851da83cfc9ca455a2218bc329
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2022-12-17 22:43:02 +0200

    upd

---
+++ src/fetchCountries.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const URL = 'https://restcountries.com/v3.1/name/';
+
+export function fetchCountries(name) {
+    return axios.get(`URL${name}?fields=name,capital,population,flags,languages`)
+        .then(({ response }) => response
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './css/styles.css';
+import _debounce from 'lodash.debounce'
 
 const DEBOUNCE_DELAY = 300;
+
+let countryName = '';
+
+import { fetchCountries } from './fetchCountries';
+fetchCountries();
+
+refs = {
+    searchQuery: document.getElementById("search-box"),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+}
+
+function onInputChange(e) {
+    if (refs.searchQuery = '') {
+        refs.countryInfo.innerHTML = '';
+        refs.countryList.innerHTML = '';
+        return;
+    }
+    e.preventDefault();
+    countryName = e.target.value.trim();
+   
+}
+refs.searchQuery.addEventListener('input', _debounce((onInputChange, DEBOUNCE_DELAY)));
