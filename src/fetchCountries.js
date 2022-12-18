ce0a9fe4c0445457cb150d8775411f5986e96c0b
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2022-12-18 20:29:01 +0200

    fixed

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 const URL = 'https://restcountries.com/v3.1/name/';
 
 export function fetchCountries(name) {
-    return axios.get(`URL${name}?fields=name,capital,population,flags,languages`)
+    return axios.get(`${URL}${name}?fields=name,capital,population,flags,languages`)
         .then((response) => response.data);
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -29,16 +29,17 @@ fetchCountries(countryName)
     .then(countries => {
         if (countries.length = 1) {
             markupClear();
+            countryInfoMarkup(countries);
         }
         else if (countries.length >= 2 || countries.length <= 10) {
             markupClear();
-            countryListMarkup();
+            countryListMarkup(countries);
         } else if (countries.length > 10) {
             markupClear();
-            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+            Notify.info('Too many matches found. Please enter a more specific name.');
         }
     })
-    .catch(Notiflix.Notify.failure("Oops, there is no country with that name"));
+    .catch(Notify.failure("Oops, there is no country with that name"));
 
 function markupClear() {
     refs.countryInfo.innerHTML = '';
